3d005f2ed68dbaba8aa4a6c32777ac6069de7263
= <=>
2021-07-10 17:05:27 +0100

    Testing Function auth

---
+++ HubBlogAssignment.UI/AuthorizationMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+{
+    public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+        NavigationManager navigationManager)
+        : base(provider, navigationManager)
+    {
+        ConfigureHandler(
+            authorizedUrls: new[] { "<your function app url>" },
+            scopes: new[] { "<the function app  API scope your define>" });
+    }
+}
--- HubBlogAssignment.UI/Program.cs
+++ HubBlogAssignment.UI/Program.cs
@@ -19,7 +19,13 @@ namespace HubBlogAssignment.UI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/")});
+            builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("ServerAPI", client =>
+              client.BaseAddress = new Uri("https://personalazurefunction.azurewebsites.net/api/"))
+                    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/")});
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+             .CreateClient("ServerAPI"));
             builder.Services.AddScoped<IPostService, PostService>();
             builder.Services.AddScoped<ICommentService, CommentService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
@@ -28,6 +34,7 @@ namespace HubBlogAssignment.UI
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://personalazurefunction.azurewebsites.net/api/");
             });
 
             await builder.Build().RunAsync();
